bdde2cad5ba4a7f8910aa8bb1b39fee81854f5ce
World Trade <normywarmy2@gmail.com>
2026-08-01 17:11:09 -0400

    Change icon filename from 'Block.ico' to 'block.ico'

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'Block.ico'),
+    icon: path.join(__dirname, 'block.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
